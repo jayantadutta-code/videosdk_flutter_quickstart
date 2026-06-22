80a4674e39a5a852ab05d6160045685d54bbe62c
jayantadutta-code <jayantadutta.india@gmail.com>
2026-06-22 13:18:02 +0530

    activate
    activate for 1 year 22/06/26

--- lib/api_call.dart
+++ lib/api_call.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 //Auth token we will use to generate a meeting and connect to it
-String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiI0YjkxNTZmZC03NDYxLTRlZTEtOTEzMy0yN2U5MGM3YjdmN2UiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTc3MTQ5NzU1OCwiZXhwIjoxNzcyMTAyMzU4fQ.2-G1kyGJCU6KxBYc-piC87ep--_3-06In7tIv--iAVY";
+String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiIyMDE0ZDg1ZC04ODQ5LTQ1N2EtOTljNy04Njc2Zjk3OGQ3NzciLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTc4MjEwOTY4NCwiZXhwIjoxNzgyNzE0NDg0fQ.1UntJWb3-eV9Wtsi31oaseWlaiIJMTLXrQAK0NtkJ2A";
 
 // API call to create meeting
 Future<String> createMeeting() async {
